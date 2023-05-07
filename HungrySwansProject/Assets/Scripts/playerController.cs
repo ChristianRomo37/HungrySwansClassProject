0b0e980af6c9b866529dfd5e1bd8ae41ff8f18d6
Colyn Reed <coreed18@gmail.com>
2023-05-07 13:37:59 -0400

    Transfered files

---
+++ HungrySwansProject/Assets/Scripts/playerController.cs
@@ -0,0 +1,135 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour, IDamage
+{
+    [Header("----- Components -----")]
+    [SerializeField] CharacterController controller;
+
+    [Header("----- Player Stats -----")]
+    [Range(1, 10)][SerializeField] int HP;
+    [Range(1, 5)][SerializeField] float playerSpeed;
+    [Range(2, 5)][SerializeField] float sprintMod;
+    [Range(8, 25)][SerializeField] float jumpHeight;
+    [Range(10, 50)][SerializeField] float gravityValue;
+    [Range(1, 3)][SerializeField] int jumpMax;
+
+    [Header("----- Weapon Stats -----")]
+    [Range(2, 300)][SerializeField] int shootDist;
+    [Range(0.1f, 3)][SerializeField] float shootRate;
+    [Range(1, 10)][SerializeField] int shootDmg;
+
+    private int timesJumped;
+    private Vector3 move;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private bool isSprinting;
+    private bool isShooting;
+    private int HPOrig;
+
+    private void Start()
+    {
+        HPOrig = HP;
+        spawn();
+    }
+
+    void Update()
+    {
+        sprint();
+
+
+        if (gameManager.instance.activeMenu == null)
+        {
+            movement();
+
+            if (Input.GetButton("Shoot") && !isShooting)
+            {
+                StartCoroutine(shoot());
+            }
+
+        }
+    }
+
+    void movement()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+            timesJumped = 0;
+        }
+
+        move = (transform.right * Input.GetAxis("Horizontal")) +
+                (transform.forward * Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && timesJumped < jumpMax)
+        {
+            timesJumped++;
+            playerVelocity.y += jumpHeight;
+        }
+
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            isSprinting = true;
+            playerSpeed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            isSprinting = false;
+            playerSpeed /= sprintMod;
+        }
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, shootDist))
+        {
+            IDamage damageable = hit.collider.GetComponent<IDamage>();
+
+            if (damageable != null)
+            {
+                damageable.takeDamage(shootDmg);
+            }
+        }
+
+        yield return new WaitForSeconds(shootRate);
+
+        isShooting = false;
+    }
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+
+        if (HP <= 0)
+        {
+            gameManager.instance.loseState();
+        }
+    }
+
+    public void spawn()
+    {
+        controller.enabled = false;
+        transform.position = gameManager.instance.playerSpawnPos.transform.position;
+        controller.enabled = true;
+        HP = HPOrig;
+    }
+    public void playerHeal(int amount)
+    {
+        HP += amount;
+    }
+}
